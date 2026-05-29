a76e3b6c2402b2ba05ae5ecf03ea8109b57ea6de
Shubhouy1 <shubhrastogi725@gmail.com>
2026-05-29 15:43:52 +0530

    added fetching data in recent section

--- src/components/Recent.tsx
+++ src/components/Recent.tsx
@@ -1,22 +1,35 @@
 import document from '../assets/document.svg'
 import documentNS from '../assets/dcoument-notselected.svg' 
-
+import { useState,useEffect } from 'react'
+import api from '../api/axios'
+import type { recentNotes } from '../types/recentNote'
+import type { GetRecentNotesResponseType } from '../types/recentNote'
 function Recent(){
+  const[recentNote , setRecentNote]= useState<recentNotes[]>([])
+    useEffect(()=>{
+      async function GetRecent(){
+        try{
+          const response = await api.get<GetRecentNotesResponseType>("/notes/recent");
+          setRecentNote(response.data.recentNotes)
+        }catch(error){
+          console.log(error)
+        }
+      }
+      GetRecent();
+    },[])
     return (
+      
     <div className='flex flex-col gap-4' >
       <span className='text-left text-white/60 text-sm font-sans pl-3 font-semibold '>Recents</span>
-      <div className='flex flex-row gap-3 bg-(--select-recent) py-3 pl-3'>
-        <img src={document} className='h-5 w-5'/>
-        <p className='text-sm font-sans text-white'>Reflection on the Month of June</p>
-      </div>
-      <div className='flex flex-row gap-3 pl-3'>
+     
+      {recentNote.map((note)=>{
+        return (
+        <div key={note.id} className='flex flex-row gap-3 pl-3'>
          <img src={documentNS} className='h-5 w-5'/>
-         <p className='text-sm font-sans text-white/60 font-semibold'>Projected Proposal</p>
+         <p className='text-sm font-sans text-white/60 font-semibold'>{note.title}</p>
       </div>
-      <div className='flex flex-row gap-3 pl-3'>
-         <img src={documentNS} className='h-5 w-5'/>
-         <p className='text-sm font-sans text-white/60 font-semibold'>Projected Proposal</p>
-      </div>
+        )
+      })}
     </div>
     )
 }
