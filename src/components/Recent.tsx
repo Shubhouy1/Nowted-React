12e8f188d3dd9c155d73a5266102312a2d66a377
Shubhouy1 <shubhrastogi725@gmail.com>
2026-05-29 18:41:53 +0530

    added conditonal rendering in recent notes

--- src/components/Recent.tsx
+++ src/components/Recent.tsx
@@ -4,9 +4,10 @@ import { useState,useEffect } from 'react'
 import api from '../api/axios'
 import type { recentNotes } from '../types/recentNote'
 import type { GetRecentNotesResponseType } from '../types/recentNote'
-import { Link } from "react-router-dom";
+import { Link ,useParams } from "react-router-dom";
 function Recent(){
   const[recentNote , setRecentNote]= useState<recentNotes[]>([])
+  const {noteId} =useParams();
     useEffect(()=>{
       async function GetRecent(){
         try{
@@ -20,15 +21,16 @@ function Recent(){
     },[])
     return (
       
-    <div className='flex flex-col gap-4' >
-      <span className='text-left text-white/60 text-sm font-sans pl-3 font-semibold '>Recents</span>
+    <div className='flex flex-col gap-1 ' >
+      <span className='text-left text-white/60 text-sm font-sans pl-3 pb-2 font-semibold '>Recents</span>
      
       {recentNote.map((note)=>{
+        const isActive = note.id === noteId;
         return (
         <Link key={note.id} to={`/notes/${note.id}`}>
-        <div className='flex flex-row gap-3 pl-3'>
-         <img src={documentNS} className='h-5 w-5'/>
-         <p className='text-sm font-sans text-white/60 font-semibold'>{note.title}</p>
+        <div className={`flex flex-row gap-3 py-2 pl-3 transition-all duration-200 ${ isActive ? "bg-(--select-recent)": ""}`}>
+         <img src={isActive ? document:documentNS} className='h-5 w-5'/>
+         <p className='text-sm font-sans text-white/60 font-semibold p-'>{note.title}</p>
         </div>
         </Link>
         )
