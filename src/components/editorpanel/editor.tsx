0c721c78a191d478f6fe9f8f8492ad870a6c122e
Shubhouy1 <shubhrastogi725@gmail.com>
2026-06-07 17:53:29 +0530

    created empty state

--- src/components/editorpanel/editor.tsx
+++ src/components/editorpanel/editor.tsx
@@ -5,7 +5,7 @@ import Title from '../editorpanel/TitleHeader'
 import { useEffect, useState } from 'react'
 import type { GetNoteResponseType, Note } from '../../types/Note'
 import api from '../../api/axios'
-
+import document_icon from '../../assets/note.svg'
 function Editorpanel(){
     const {noteId} = useParams()
     const [note ,setNote] = useState<Note| null>(null)
@@ -23,8 +23,21 @@ function Editorpanel(){
         }
         fetchNote()
     },[noteId])
+
+    if(!noteId){
+        return (
+           <div className='flex flex-col justify-center items-center gap-2 h-screen'>
+             <img src={document_icon} className='w-20 h-20'></img>
+             <div className='flex flex-col justify-center items-center gap-2'>
+             <p className='font-semibold text-white text-2xl'>Select a note to view</p>
+             <p className='font-semibold text-white/60 text-sm'>Choose a note from the list on the left to view its contents, or create a </p>
+             <p className='font-semibold text-white/60 text-sm'>new note to add to your collection</p>
+             </div>
+           </div>
+        )
+    }
     return (
-        <div className='h-full overflow-y-auto w-full flex flex-col gap-5'>
+        <div className='h-screen overflow-y-auto w-full flex flex-col gap-5'>
          <Title titleText={note?.title}/>
          <Info createdAt ={note?.createdAt} folder ={note?.folder?.name}/>
          <Description content ={note?.content}/>
