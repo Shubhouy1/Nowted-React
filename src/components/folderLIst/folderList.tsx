82b96a9875da1ba015f5bba57060b3f0bdba9f1d
Shubhouy1 <shubhrastogi725@gmail.com>
2026-06-11 17:37:46 +0530

    toggle between different sections of more

--- src/components/folderLIst/folderList.tsx
+++ src/components/folderLIst/folderList.tsx
@@ -12,6 +12,7 @@ function FolderList ({folderName,refreshNotes}:folderListProps){
   const {folderId,noteId} = useParams()
   const [notes,setNotes] = useState<Note[]>([])
   const location = useLocation()
+  console.log(location.pathname)
   const isFavoritePage = location.pathname.startsWith("/favorites")
   const isArchivedPage = location.pathname.startsWith("/archived")
   const isTrashPage = location.pathname.startsWith("/trash")
@@ -19,7 +20,7 @@ function FolderList ({folderName,refreshNotes}:folderListProps){
     console.log(refreshNotes)
     async function getNotes(){
       try{
-        let url =""
+        let url =`/notes?folderId=${folderId}`
         if(isFavoritePage){
           url="/notes?favorite=true"
         }
@@ -38,18 +39,28 @@ function FolderList ({folderName,refreshNotes}:folderListProps){
       }
     }
     getNotes();
-  },[folderId,refreshNotes])
+  },[folderId,refreshNotes,isArchivedPage,isFavoritePage,isTrashPage])
 
     return (
         <div className='flex flex-col bg-(--folder-section) h-full gap-5'>
         <div className='pt-5 pl-5'>
-            <p className="text-[22px] font-semibold leading-none text-white">{folderName}</p>
+            <p className="text-[22px] font-semibold leading-none text-white">{isTrashPage?"Trash" : isArchivedPage ?"Archived" : isFavoritePage? "Favorite" : folderName}</p>
         </div >
         <div className="px-5 flex flex-col gap-4">
         {notes.map((note)=>{
          const isActive = noteId ===note.id
+         let notePath =`/folders/${folderId}/notes/${note.id}`
+         if(isFavoritePage){
+          notePath =`/favorites/notes/${note.id}`
+         }
+         if(isTrashPage){
+          notePath =`/trash/notes/${note.id}`
+         }
+         if(isArchivedPage){
+          notePath =`/archived/notes/${note.id}`
+         }
          return(
-        <Link key={note.id} to={`/folders/${folderId}/notes/${note.id}`}>
+        <Link key={note.id} to={notePath}>
          <Notecard 
           title={note.title}
           date={note.createdAt}
